dcfa302ec593bdee6a59fcabd3de5c7cfc4e9792
LoicVinci <loic.hernaut@student.vinci.be>
2022-11-02 14:34:12 +0100

    Add Doc Drivers and Create 3 micro service module

---
+++ drivers/src/test/java/be/vinci/ipl/chattycar/drivers/DriversApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.chattycar.drivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

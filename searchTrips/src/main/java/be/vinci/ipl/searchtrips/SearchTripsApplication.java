2bc08f0ddd5f2fe8e5d6b521e2cdba83f3273412
flolic16 <filip.lolic@student.vinci.be>
2022-11-12 11:43:21 +0100

    SearchTripls micro  Yaml in progress

---
+++ searchTrips/src/main/java/be/vinci/ipl/searchtrips/SearchTripsApplication.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.searchtrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchTripsApplication.class, args);
+    }
+
+}

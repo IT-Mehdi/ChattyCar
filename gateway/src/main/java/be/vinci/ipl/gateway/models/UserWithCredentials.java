e8219ec9c255dc4e6fe96e25a7c7a2da22e1160a
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-11-21 14:35:38 +0100

    Add UserWithCredentials in models

---
+++ gateway/src/main/java/be/vinci/ipl/gateway/models/UserWithCredentials.java
@@ -0,0 +1,25 @@
+package be.vinci.ipl.gateway.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserWithCredentials {
+
+  private String email;
+  private String firstname;
+  private String lastname;
+  private String password;
+
+  public User toUser() {
+    return new User(email, firstname, lastname);
+  }
+  public Credentials toCredentials() {
+    return new Credentials(email, password);
+  }
+}

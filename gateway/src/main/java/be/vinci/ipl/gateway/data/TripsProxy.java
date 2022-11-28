aadf4c09c56c239093faf224242704808c6b5c22
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-11-28 14:31:31 +0100

    implement tripsProxy

---
+++ gateway/src/main/java/be/vinci/ipl/gateway/data/TripsProxy.java
@@ -0,0 +1,37 @@
+package be.vinci.ipl.gateway.data;
+
+import be.vinci.ipl.gateway.models.NewUser;
+import be.vinci.ipl.gateway.models.Trip;
+import be.vinci.ipl.gateway.models.User;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+@Repository
+@FeignClient(name = "trips")
+public interface TripsProxy {
+
+  @PostMapping("/trips")
+  Trip createTrip(@RequestBody Trip trip);
+
+  @GetMapping("/trips")
+  List<Trip> readTrips(@RequestParam String departure_date, @RequestParam double originLat,
+      @RequestParam double originLon,
+      @RequestParam double destinationLat, @RequestParam double destinationLon);
+
+  @GetMapping("/trips/{id}")
+  Trip readTripById(@PathVariable int id);
+
+  @DeleteMapping("/trips/{id}")
+  void deleteTripById(@PathVariable int id);
+
+  @GetMapping("/trips/driver/{id}")
+  List<Trip> readTripsByDriver(@PathVariable int id);
+
+  @DeleteMapping("/trips/driver/{id}")
+  void deleteTripsByDriver(@PathVariable int id);
+
+  @GetMapping("/trips/passengers/{id}")
+  List<Trip> readTripsByPassenger(@PathVariable int id);
+}

73e6513ed0faf1e8b54895556c9ef55a15d2d3b3
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-11-29 14:48:14 +0100

    Add inscriptions proxy

---
+++ gateway/src/main/java/be/vinci/ipl/gateway/data/InscriptionProxy.java
@@ -0,0 +1,42 @@
+package be.vinci.ipl.gateway.data;
+
+import be.vinci.ipl.gateway.models.Inscription;
+import be.vinci.ipl.gateway.models.PassengerTrips;
+import be.vinci.ipl.gateway.models.Passengers;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Repository
+@FeignClient(name = "inscriptions")
+public interface InscriptionProxy {
+
+  @GetMapping("/inscriptions/{trip_id}")
+  Passengers readAllPassengersTrip(@PathVariable("trip_id") int tripId);
+
+  @DeleteMapping("/inscriptions/{trip_id}")
+  void deleteAllPassengersTrip(@PathVariable("trip_id") int tripId);
+
+  @GetMapping("/inscriptions/passengers/{passenger_id}")
+  PassengerTrips readAllTripsPassenger(@PathVariable("passenger_id") int passengerId);
+
+  @DeleteMapping("/inscriptions/passengers/{passenger_id}")
+  void deleteAllTripsPassenger(@PathVariable("passenger_id") int passengerId);
+
+  @PostMapping("/inscriptions/{trip_id}/{passenger_id}")
+  Inscription createInscription(@PathVariable("trip_id") int tripId, @PathVariable("passenger_id") int passengerId);
+
+  @GetMapping("/inscriptions/{trip_id}/{passenger_id}")
+  Inscription readInscription(@PathVariable("trip_id") int tripId, @PathVariable("passenger_id") int passengerId);
+
+  @DeleteMapping("/inscriptions/{trip_id}/{passenger_id}")
+  void deleteInscription(@PathVariable("trip_id") int tripId, @PathVariable("passenger_id") int passengerId);
+
+  @PutMapping("/inscriptions/{trip_id}/{passenger_id}")
+  void updateInscription(@PathVariable("trip_id") int tripId, @PathVariable("passenger_id") int passengerId);
+
+}

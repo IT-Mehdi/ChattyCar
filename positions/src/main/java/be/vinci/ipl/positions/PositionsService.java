77039a4d5201ee5327cde829471f2f4ef54928bf
LoicVinci <loic.hernaut@student.vinci.be>
2022-12-02 02:05:49 +0100

    Fix Position Error

--- positions/src/main/java/be/vinci/ipl/positions/PositionsService.java
+++ positions/src/main/java/be/vinci/ipl/positions/PositionsService.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Service;
 public class PositionsService {
 
     public double calculateDistance(double originLatitude, double originLongitude, double destLatitude, double destLongitude) {
+        double lat1 = Math.toRadians(originLatitude);
+        double long1 = Math.toRadians(originLongitude);
+        double lat2 = Math.toRadians(destLatitude);
+        double long2 = Math.toRadians(destLongitude);
 
         double earthRadius = 6371.01; //Kilometers
-        return earthRadius * Math.acos(Math.sin(originLatitude)*Math.sin(destLatitude) + Math.cos(originLatitude)*Math.cos(destLatitude)*Math.cos(originLongitude - destLongitude));
+        return earthRadius * Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2)*Math.cos(long1 - long2));
     }
 }
